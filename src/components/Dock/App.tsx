4192a424811b713fb898978e223a61778888aae0
Ricardo Dias <ricardodiask9@outlook.com>
2024-08-28 15:15:24 +0100

    OS-18(feature): dock apps tooltips (#17)

--- src/components/Dock/App.tsx
+++ src/components/Dock/App.tsx
@@ -1,11 +1,16 @@
+import { Tooltip } from "react-tooltip";
+
+import { useLabels } from "src/services/client";
 import { useAppsUpdate } from "src/context";
 
 import type { AppProps } from "./types";
 
 const StyledApp =
   "flex items-center h-full px-3 cursor-pointer bg-white/50 data-[minimized=true]:bg-white/25 hover:data-[minimized=true]:bg-white/50";
+const StyledTooltip = "!bg-[#212121]";
 
 const App = ({ app }: AppProps) => {
+  const getLabel = useLabels();
   const updateApps = useAppsUpdate();
   const { id, Icon } = app;
 
@@ -21,14 +26,27 @@ const App = ({ app }: AppProps) => {
     });
   };
 
+  const tooltipId = `app-${app.id}-dock-shortcut`;
+
   return (
-    <div
-      className={StyledApp}
-      onClick={maximizeApp}
-      data-minimized={app.minimized}
-    >
-      <Icon size="36" />
-    </div>
+    <>
+      <div
+        id={tooltipId}
+        className={StyledApp}
+        onClick={maximizeApp}
+        data-minimized={app.minimized}
+      >
+        <Icon size="36" />
+      </div>
+      <Tooltip
+        noArrow
+        delayShow={300}
+        className={StyledTooltip}
+        anchorSelect={`#${tooltipId}`}
+      >
+        <p>{getLabel(app.name)}</p>
+      </Tooltip>
+    </>
   );
 };
 
