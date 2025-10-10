8ae4e3a32a04008474ea2375bd065d31a08428cc
Ricardo Dias <ricardodiask9@outlook.com>
2025-10-10 14:33:51 +0000

    OS-2(feature): add github icon

--- src/shared/components/index.tsx
+++ src/shared/components/index.tsx
@@ -5,6 +5,7 @@ import LanguageButton from "./LanguageButton";
 import Modal from "./Modal";
 import AboutProjectModal from "./Modal/AboutProject";
 import HelpTooltip from "./HelpTooltip";
+import GitHub from "./GitHub";
 
-export { Modal, AboutProjectModal, Input, Loading, Tooltip, LanguageButton, HelpTooltip };
+export { Modal, AboutProjectModal, Input, Loading, Tooltip, LanguageButton, HelpTooltip, GitHub };
 export * from "./Icons";
